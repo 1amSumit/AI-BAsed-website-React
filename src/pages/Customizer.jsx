297a5cc063e9a9a87644c30fc1d5535869a98f5a
1amSumit <sj779619@gmail.com>
2023-04-06 19:53:01 +0530

    Three js implemented

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -27,13 +27,39 @@ const Customizer = () => {
             className="ablosute top-0 left-0 z-10"
             {...slideAnimation("left")}
           >
-            <div className="flex items-centre min-h-screen">
+            <div className="flex items-center min-h-screen">
               <div className="editorTabs tabs">
                 {EditorTabs.map((tab) => (
-                  <Tabs key="tab" />
+                  <Tabs key={tab.name} tab={tab} handleClick={() => {}} />
                 ))}
               </div>
             </div>
+          </motion.div>
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CoustomButton
+              type="filled"
+              title="GO BACK"
+              handleClick={() => (state.intro = true)}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation("up")}
+          >
+            {FilterTabs.map((tab) => (
+              <Tabs
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=""
+                handleClick={() => {}}
+              />
+            ))}
           </motion.div>
         </>
       )}
